d7c8882088ddbafce50befa45294809b645757af
yawok <obengky72@gmail.com>
2022-03-14 16:04:43 +0000

    added page to display contents of posts.

--- blogposts/urls.py
+++ blogposts/urls.py
@@ -8,5 +8,10 @@ app_name = "blogposts"
 urlpatterns = [
         #home page
         path("", views.index, name="index"),
-
+        #Display all posts
+        path("posts/", views.posts, name="posts"),
+        #Display the content of a post
+        path("posts/<int:post_id>/", views.post, name="post"),
+        #Form to add new posts
+        path("posts/new_post/", views.new_post, name="post"),
         ]
--- blogposts/views.py
+++ blogposts/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 
+from .models import Post
 
 def index(request):
     """The home page"""
     return render(request, "blogposts/index.html")
+
+
+def posts(request):
+    """Page to display all the posts."""
+    posts = Post.objects.order_by("date")
+    context = {"posts": posts}
+    return render(request, "blogposts/posts.html", context)
+
+
+def post(request, post_id):
+    """Page to display content of a post."""
+    title = Post.objects.get(id=post_id)
+    content = title.post
+    context = {"title": title, "content": content}
+    return render(request, "blogposts/post.html", context)
