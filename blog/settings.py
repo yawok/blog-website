151703202bc5a3db200365301a7c8f42765f1bac
yaw.o.k <obengky72@gmail.com>
2022-06-10 11:55:23 +0000

    Remove hard coded Secret Key

--- blog/settings.py
+++ blog/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kv0qx+6$gnyf@1ly=zfzi6pl^a)g7-mh7fq7e^6ii43jxur8+@'
+SECRET_KEY = config("SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
